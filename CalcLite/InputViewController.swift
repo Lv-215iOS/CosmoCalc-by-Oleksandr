053106285404351fa1d3033a1fe4ff972c8bed41
Oleksandr <adminaccount@macminissu1.training.local>
2016-12-15 21:57:13 +0200

    CalcTestModelContainers

---
+++ CalcLite/InputViewController.swift
@@ -0,0 +1,57 @@
+//
+//  InputViewController.swift
+//  CalcLite
+//
+//  Created by adminaccount on 12/14/16.
+//  Copyright © 2016 adminaccount. All rights reserved.
+//
+
+import UIKit
+
+protocol InputInterface {
+    var buttonDidPress: ((_ operation: String)->())? {get set}
+}
+
+class InputViewController: UIViewController, InputInterface {
+    
+    var mainVC: ViewController? = nil
+    
+    var buttonDidPress: ((String) -> ())? = nil
+    
+    func pressDigit(_ sender: UIButton) {
+        buttonDidPress?(sender.currentTitle!)
+    }
+    
+    func binaryOperationPressed(_ sender: UIButton){
+        buttonDidPress?(sender.currentTitle!)
+    }
+    
+    func equalOperationPressed(_ sender: UIButton) {
+        buttonDidPress?(sender.currentTitle!)
+    }
+
+
+    
+    
+    /*override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }*/
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
